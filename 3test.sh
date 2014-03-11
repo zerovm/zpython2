103ad4fc3bbb3d914631c886dcafea8269783abe
Sergei Turukin <rampage644@gmail.com>
2014-03-11 13:10:46 +0200

    Refactoring
    Moved with() block outside main cycle to make test list more flexible.

--- 3test.sh
+++ 3test.sh
@@ -7,19 +7,22 @@ import subprocess
 FILE = 'tests.txt'
 devnull = open(os.devnull, "w")
 
+# tests from external file
 with open(FILE, 'r') as rfile:
-    for test in rfile:
-        print("%s..." % test.strip()),
-        sys.stdout.flush()
-        try:
-            ret = subprocess.call(['zvsh', '--zvm-image', 'python.tar',
-                                   'python', '-mtest.regrtest', test],
-                                  stdout=devnull, stderr=devnull)
-        except KeyboardInterrupt:
-            break
-        if ret:
-            print('\033[1;31mfail\033[1;m')
-        else:
-            print('\033[1;32mok\033[1;m')
+    tests = rfile.read().splitlines()
+
+for test in tests:
+    print("%s..." % test.strip()),
+    sys.stdout.flush()
+    try:
+        ret = subprocess.call(['zvsh', '--zvm-image', 'python.tar',
+                               'python', '-mtest.regrtest', test],
+                              stdout=devnull, stderr=devnull)
+    except KeyboardInterrupt:
+        break
+    if ret:
+        print('\033[1;31mfail\033[1;m')
+    else:
+        print('\033[1;32mok\033[1;m')
 
 devnull.close()
